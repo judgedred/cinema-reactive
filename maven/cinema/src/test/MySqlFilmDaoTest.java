646d5d3ea26b02a5c18c685a1a56c61b078466c1
Oleg <D:\The Bat!\Mail>
2015-06-06 16:54:06 +0300

    started working on junit

---
+++ maven/cinema/src/test/MySqlFilmDaoTest.java
@@ -0,0 +1,65 @@
+package test;
+
+import com.dao.*;
+import com.domain.*;
+import com.mysql.*;
+import java.util.*;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class MySqlFilmDaoTest
+{
+	@Test
+	public void testGetFilmById() throws DaoException
+	{
+		DaoFactory daoFactory = new MySqlDaoFactory();
+		FilmDao filmDao = daoFactory.getFilmDao();
+		Film filmTest = filmDao.getFilmById("1");
+		Assert.assertNotNull(filmTest);
+	}	
+
+	@Test
+	public void testCreate() throws DaoException
+	{
+		DaoFactory daoFactory = new MySqlDaoFactory();
+		FilmDao filmDao = daoFactory.getFilmDao();
+		Film film = new Film();
+		film.setFilmName("Интерстеллар");
+		film.setDescription("Сквозь пространство и время");
+		String filmNameExpected = film.getFilmName();
+		String descriptionExpected = film.getDescription();
+		Film filmTest = filmDao.create(film);
+		Assert.assertNotNull(filmTest);
+		String filmNameResult = filmTest.getFilmName();
+		String descriptionResult = filmTest.getDescription();	
+		Assert.assertEquals(filmNameExpected, filmNameResult);
+		Assert.assertEquals(descriptionExpected, descriptionResult);
+	}
+	
+	@Test
+	public void testUpdate() throws DaoException
+	{
+		DaoFactory daoFactory = new MySqlDaoFactory();
+		FilmDao filmDao = daoFactory.getFilmDao();
+		Film film = new Film();
+		film.setFilmId(2);
+		film.setFilmName("Кибер")ж
+		film.setDescription("Экшн");
+		String filmNameExpected = film.getFilmName();
+		String descriptionExpected = film.getDescription();
+		filmDao.update(film);
+		Film filmTest = filmDao.getFilmById(film.getFilmId());
+		Assert.assertNotNull(filmTest);	
+		String filmNameResult = filmTest.getFilmName();
+		String descriptionResult = filmTest.getDescription();
+		Assert.assertEquals(filmNameExpected, filmNameResult);
+		Assert.assertEquals(descriptionExpected, descriptionResult);
+	}
+
+	@Test
+	public void testDelete() throws DaoException
+	{
+		
+	}
+}
