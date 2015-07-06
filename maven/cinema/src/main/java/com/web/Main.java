d771fcb0bf4c26701ead7ff5f14ce3e4720f7800
Oleg <89-oleg-2006@rambler.ru>
2015-07-06 18:04:28 +0300

    done autowire in servlet. try autowire in session listener? do smth with filepath to beans.xml.

--- maven/cinema/src/main/java/com/web/Main.java
+++ maven/cinema/src/main/java/com/web/Main.java
@@ -2,8 +2,13 @@ package com.web;
 
 import com.dao.*;
 import com.domain.*;
+import com.mysql.MySqlFilmshowDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.io.PrintWriter;
 import java.util.*;
@@ -15,7 +20,7 @@ import java.io.IOException;
 public class Main extends HttpServlet
 {
     @Autowired
-    private FilmshowDao filmshowDao;
+    private MySqlFilmshowDao filmshowDao;
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -26,6 +31,8 @@ public class Main extends HttpServlet
         out.println("<head><title>Test</title></head>");
         out.println("<body>");
         out.println("<h1>Testing the Servlet</h1>");
+//        ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+//        filmshowDao = context.getBean("mySqlFilmshowDao", MySqlFilmshowDao.class);
 
 //		HttpSession session = request.getSession();
 		try
@@ -50,5 +57,11 @@ public class Main extends HttpServlet
 //        RequestDispatcher dispatcher = request.getRequestDispatcher("Main.jsp");
 //		dispatcher.forward(request, response);
 	}
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
+        super.init(config);
+    }
 }
 
