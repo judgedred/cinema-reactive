9df354045ec7281e47994dd083e28b9c6e0b5c3f
Oleg <89-oleg-2006@rambler.ru>
2015-07-08 17:16:08 +0300

    started redoing servlet mapping

--- maven/cinema/src/main/java/com/web/Main.java
+++ maven/cinema/src/main/java/com/web/Main.java
@@ -25,38 +25,42 @@ public class Main extends HttpServlet
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-        /*response.setContentType("text/html");
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head><title>Test</title></head>");
         out.println("<body>");
-        out.println("<h1>Testing the Servlet</h1>");*/
+        out.println("<h1>Testing the Servlet</h1>");
+
 //        ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
 //        filmshowDao = context.getBean("mySqlFilmshowDao", MySqlFilmshowDao.class);
-
 		HttpSession session = request.getSession();
-		try
-			{												
+        if(Objects.equals(request.getPathInfo(), "/Main"))
+        {
+            out.println(request.getPathInfo());
+            try
+            {
 //	 			FilmshowDao filmshowDao = (FilmshowDao)session.getAttribute("filmshowDao");
                 session.setAttribute("filmshowDao", filmshowDao);
-	 			List<Filmshow> ls = filmshowDao.getFilmshowAll();
-				session.setAttribute("filmshowList", ls);
+                List<Filmshow> ls = filmshowDao.getFilmshowAll();
+                session.setAttribute("filmshowList", ls);
 
-               /* for(Iterator<Filmshow> i = ls.iterator(); i.hasNext(); )
+               for(Iterator<Filmshow> i = ls.iterator(); i.hasNext(); )
                 {
                     Filmshow f = i.next();
                     out.println("<h2>inside iterator</h2>" + " " + f);
 
-                }*/
-			}												
-			catch(Exception e)										
-			{												
-		 		e.printStackTrace();
-			}
-       /* out.println("</body></html>");
-        out.close();*/
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Main.jsp");
-		dispatcher.forward(request, response);
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+       out.println("</body></html>");
+        out.close();
+            RequestDispatcher dispatcher = request.getRequestDispatcher("Main.jsp");
+            dispatcher.forward(request, response);
+        }
 	}
 
     @Override
