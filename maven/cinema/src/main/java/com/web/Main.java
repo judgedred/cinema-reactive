a9875744b0996c9769bbdcc01c38fa1a7bc7bd03
Oleg <89-oleg-2006@rambler.ru>
2015-07-08 23:54:10 +0300

    finished integration spring in servlets, made one Main servlet

--- maven/cinema/src/main/java/com/web/Main.java
+++ maven/cinema/src/main/java/com/web/Main.java
@@ -1,16 +1,12 @@
 package com.web;
 
-import com.dao.*;
 import com.domain.*;
-import com.mysql.MySqlFilmshowDao;
+import com.mysql.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -22,46 +18,171 @@ public class Main extends HttpServlet
     @Autowired
     private MySqlFilmshowDao filmshowDao;
 
+    @Autowired
+    private MySqlFilmDao filmDao;
+
+    @Autowired
+    private MySqlUserDao userDao;
+
+    @Autowired
+    private MySqlHallDao hallDao;
+
+    @Autowired
+    private MySqlReservationDao reservationDao;
+
+    @Autowired
+    private MySqlSeatDao seatDao;
+
+    @Autowired
+    private MySqlTicketDao ticketDao;
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head><title>Test</title></head>");
-        out.println("<body>");
-        out.println("<h1>Testing the Servlet</h1>");
+        String url = request.getServletPath();
+        HttpSession session = request.getSession();
 
-//        ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-//        filmshowDao = context.getBean("mySqlFilmshowDao", MySqlFilmshowDao.class);
-		HttpSession session = request.getSession();
-        if(Objects.equals(request.getPathInfo(), "/Main"))
+        if(url.equals("/Main"))
         {
-            out.println(request.getPathInfo());
             try
             {
-//	 			FilmshowDao filmshowDao = (FilmshowDao)session.getAttribute("filmshowDao");
                 session.setAttribute("filmshowDao", filmshowDao);
                 List<Filmshow> ls = filmshowDao.getFilmshowAll();
                 session.setAttribute("filmshowList", ls);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
 
-               for(Iterator<Filmshow> i = ls.iterator(); i.hasNext(); )
+            RequestDispatcher dispatcher = request.getRequestDispatcher("Main.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        if(url.equals("/AddFilm"))
+        {
+            try
+            {
+                session.setAttribute("filmDao", filmDao);
+                String filmName = request.getParameter("filmName");
+                String description = request.getParameter("description");
+                Film film = new Film();
+                if(filmName != null && !filmName.isEmpty() && description != null && !description.isEmpty())
                 {
-                    Filmshow f = i.next();
-                    out.println("<h2>inside iterator</h2>" + " " + f);
-
+                    film.setFilmName(filmName);
+                    film.setDescription(description);
+                    filmDao.create(film);
                 }
             }
             catch(Exception e)
             {
                 e.printStackTrace();
             }
-       out.println("</body></html>");
-        out.close();
-            RequestDispatcher dispatcher = request.getRequestDispatcher("Main.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("AddFilm.jsp");
             dispatcher.forward(request, response);
         }
-	}
+
+        if(url.equals("/AddFilmshow"))
+        {
+            try
+            {
+                session.setAttribute("filmshowDao", filmshowDao);
+                session.setAttribute("filmDao", filmDao);
+                session.setAttribute("hallDao", hallDao);
+
+                List<Film> filmList = filmDao.getFilmAll();
+                List<Hall> hallList = hallDao.getHallAll();
+                session.setAttribute("filmList", filmList);
+                session.setAttribute("hallList", hallList);
+                int filmId = Integer.parseInt(request.getParameter("filmSelect"));
+                int hallId = Integer.parseInt(request.getParameter("hallSelect"));
+                Date dateTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'").parse(request.getParameter("dateTime"));
+                Film film = filmDao.getFilmById(filmId);
+                Hall hall = hallDao.getHallById(hallId);
+                Filmshow filmshow = new Filmshow();
+                if(film != null && hall != null && dateTime != null)
+                {
+                    filmshow.setFilm(film);
+                    filmshow.setHall(hall);
+                    filmshow.setDateTime(dateTime);
+                    filmshowDao.create(filmshow);
+                }
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("AddFilmshow.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        if(url.equals("/DeleteFilmshow"))
+        {
+            try
+            {
+                session.setAttribute("filmshowDao", filmshowDao);
+
+                int filmshowId = Integer.parseInt(request.getParameter("filmshowSelect"));
+                Filmshow filmshow = filmshowDao.getFilmshowById(filmshowId);
+                if(filmshow != null)
+                {
+                    filmshowDao.delete(filmshow);
+                }
+                List<Filmshow> ls = filmshowDao.getFilmshowAll();
+                session.setAttribute("filmshowList", ls);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("DeleteFilmshow.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        if(url.equals("/Register"))
+        {
+            try
+            {
+                session.setAttribute("userDao", userDao);
+
+                User user = new User();
+                String login = request.getParameter("login");
+                String password = request.getParameter("password");
+                String email = request.getParameter("email");
+                if(login != null && !login.isEmpty() && password != null && !password.isEmpty() && email != null && !email.isEmpty())
+                {
+                    user.setLogin(login);
+                    user.setPassword(password);
+                    user.setEmail(email);
+                    userDao.create(user);
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("Register.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        if(url.equals("/UserList"))
+        {
+            try
+            {
+                session.setAttribute("userDao", userDao);
+
+                List<User> ls = userDao.getUserAll();
+                session.setAttribute("UserList", ls);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("UserList.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 
     @Override
     public void init(ServletConfig config) throws ServletException
