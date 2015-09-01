0c6290708ef3de2bcc9bf4bb928d9617588fd59a
Oleg <89-oleg-2006@rambler.ru>
2015-09-01 22:46:49 +0300

    returned tests to database dependent with some cleanup

--- maven/cinema/src/test/java/MySqlFilmshowDaoTest.java
+++ maven/cinema/src/test/java/MySqlFilmshowDaoTest.java
@@ -2,8 +2,6 @@ import com.dao.*;
 import com.domain.*;
 import com.mysql.*;
 import java.util.*;
-
-import org.junit.After;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
@@ -30,155 +28,77 @@ public class MySqlFilmshowDaoTest
     @Autowired
     private HallDao hallDao;
 
-	@Test
-	public void testGetFilmshowById() throws DaoException
-	{
-		Filmshow filmshowTest = filmshowDao.getFilmshowById(1);
-		Assert.assertNotNull(filmshowTest);
-	}	
-
-	@Test
-	public void testCreate() throws DaoException
-	{
-        Film film = new Film();
-        film.setFilmName("filmForCreate");
-        film.setDescription("filmForCreate");
-        Film filmForCreate = filmDao.create(film);
-        Hall hall = new Hall();
-        hall.setHallNumber(10);
-        hall.setHallName("hallForCreate");
-        Hall hallForCreate = hallDao.create(hall);
-		Filmshow filmshow = new Filmshow();
-		filmshow.setFilm(filmForCreate);
-		filmshow.setHall(hallForCreate);
-		Calendar cal = Calendar.getInstance();
-		cal.set(2015, Calendar.JUNE, 8, 21, 30);
-		filmshow.setDateTime(cal.getTime());
-		Film filmExpected = filmshow.getFilm();
-		Hall hallExpected = filmshow.getHall();
-		String dateTimeExpected = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshow.getDateTime());
-		Filmshow filmshowTest = filmshowDao.create(filmshow);
-		Assert.assertNotNull(filmshowTest);
-		Film filmResult = filmshowTest.getFilm();
-		Hall hallResult = filmshowTest.getHall();
-		String dateTimeResult = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshowTest.getDateTime());
-		Assert.assertEquals(filmExpected, filmResult);
-		Assert.assertEquals(hallExpected, hallResult);
-		Assert.assertEquals(dateTimeExpected, dateTimeResult);
-	}
-	
-	@Test
-	public void testUpdate() throws DaoException
-	{
-        Film film = new Film();
-        film.setFilmName("filmForUpdate");
-        film.setDescription("filmForUpdate");
-        Film filmForUpdate = filmDao.create(film);
-        Hall hall = new Hall();
-        hall.setHallNumber(10);
-        hall.setHallName("hallForUpdate");
-        Hall hallForUpdate = hallDao.create(hall);
-        Filmshow filmshow = new Filmshow();
-        filmshow.setFilm(filmForUpdate);
-        filmshow.setHall(hallForUpdate);
-        Calendar cal = Calendar.getInstance();
-        cal.set(2015, Calendar.JUNE, 7, 20, 0);
-        filmshow.setDateTime(cal.getTime());
-        Filmshow filmshowForUpdate = filmshowDao.create(filmshow);
-        film.setFilmName("filmUpdatePassed");
-        film.setDescription("filmUpdatePassed");
-        Film filmUpdatePassed = filmDao.create(film);
-        hall.setHallNumber(11);
-        hall.setHallName("hallUpdatePassed");
-        Hall hallUpdatePassed = hallDao.create(hall);
-		filmshow.setFilmshowId(filmshowForUpdate.getFilmshowId());
-		filmshow.setFilm(filmUpdatePassed);
-		filmshow.setHall(hallUpdatePassed);
-		cal.set(2015, Calendar.JUNE, 8, 20, 0);
-		filmshow.setDateTime(cal.getTime());
-		Film filmExpected = filmshow.getFilm();
-		Hall hallExpected = filmshow.getHall();
-		String dateTimeExpected = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshow.getDateTime());
-		filmshowDao.update(filmshow);
-		Filmshow filmshowTest = filmshowDao.getFilmshowById(filmshowForUpdate.getFilmshowId());
-		Assert.assertNotNull(filmshowTest);
-		Film filmResult = filmshowTest.getFilm();
-		Hall hallResult = filmshowTest.getHall();
-		String dateTimeResult = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshowTest.getDateTime());
-		Assert.assertEquals(filmExpected, filmResult);
-		Assert.assertEquals(hallExpected, hallResult);
-		Assert.assertEquals(dateTimeExpected, dateTimeResult);
-	}
-
-	@Test
-	public void testDelete() throws DaoException
-	{
-        Film film = new Film();
-        film.setFilmName("filmForDelete");
-        film.setDescription("filmForDelete");
-        Film filmForDelete = filmDao.create(film);
-        Hall hall = new Hall();
-        hall.setHallNumber(10);
-        hall.setHallName("hallForDelete");
-        Hall hallForDelete = hallDao.create(hall);
-        Filmshow filmshow = new Filmshow();
-        filmshow.setFilm(filmForDelete);
-        filmshow.setHall(hallForDelete);
-        Calendar cal = Calendar.getInstance();
-        cal.set(2015, Calendar.JUNE, 7, 20, 0);
-        filmshow.setDateTime(cal.getTime());
-        Filmshow filmshowForDelete = filmshowDao.create(filmshow);
-		filmshowDao.delete(filmshowForDelete);
-		Assert.assertNull(filmshowDao.getFilmshowById(filmshowForDelete.getFilmshowId()));
-	}
-
-	@Test
-	public void testGetFilmshowAll() throws DaoException
-	{
-		List<Filmshow> listTest = filmshowDao.getFilmshowAll();
-		Assert.assertNotNull(listTest);
-		Assert.assertTrue(listTest.size() > 0);
-	}
-
-    @After
-    public void cleanUp()
+    @Test
+    public void testGetFilmshowById() throws DaoException
     {
-        try
-        {
-            List<Filmshow> filmshowLst = filmshowDao.getFilmshowAll();
-            for(Filmshow f : filmshowLst)
-            {
-                if(f.getFilm().getFilmName().equals("filmForCreate") || f.getFilm().getFilmName().equals("filmUpdatePassed"))
-                {
-                    filmshowDao.delete(f);
-                }
-            }
-            List<Film> filmLst = filmDao.getFilmAll();
-            for(Film f : filmLst)
-            {
-                if(f.getFilmName().equals("filmForCreate")
-                        || f.getFilmName().equals("filmForUpdate")
-                        || f.getFilmName().equals("filmUpdatePassed")
-                        || f.getFilmName().equals("filmForDelete"))
-                {
-                    filmDao.delete(f);
-                }
-            }
-            List<Hall> hallLst = hallDao.getHallAll();
-            for(Hall h : hallLst)
-            {
-                if(h.getHallName().equals("hallForCreate")
-                        || h.getHallName().equals("hallForUpdate")
-                        || h.getHallName().equals("hallUpdatePassed")
-                        || h.getHallName().equals("hallForDelete"))
-                {
-                    hallDao.delete(h);
-                }
-            }
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+        Filmshow filmshowTest = filmshowDao.getFilmshowById(1);
+        Assert.assertNotNull(filmshowTest);
+    }
+
+    @Test
+    public void testCreate() throws DaoException
+    {
+        Filmshow filmshow = new Filmshow();
+        Film film = filmDao.getFilmById(1);
+        Hall hall = hallDao.getHallById(1);
+        filmshow.setFilm(film);
+        filmshow.setHall(hall);
+        Calendar cal = Calendar.getInstance();
+        cal.set(2015, Calendar.JUNE, 8, 21, 30);
+        filmshow.setDateTime(cal.getTime());
+        Film filmExpected = filmshow.getFilm();
+        Hall hallExpected = filmshow.getHall();
+        String dateTimeExpected = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshow.getDateTime());
+        Filmshow filmshowTest = filmshowDao.create(filmshow);
+        Assert.assertNotNull(filmshowTest);
+        Film filmResult = filmshowTest.getFilm();
+        Hall hallResult = filmshowTest.getHall();
+        String dateTimeResult = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshowTest.getDateTime());
+        Assert.assertEquals(filmExpected, filmResult);
+        Assert.assertEquals(hallExpected, hallResult);
+        Assert.assertEquals(dateTimeExpected, dateTimeResult);
+    }
+
+    @Test
+    public void testUpdate() throws DaoException
+    {
+        Filmshow filmshow = new Filmshow();
+        Film film = filmDao.getFilmById(2);
+        Hall hall = hallDao.getHallById(2);
+        filmshow.setFilmshowId(2);
+        filmshow.setFilm(film);
+        filmshow.setHall(hall);
+        Calendar cal = Calendar.getInstance();
+        cal.set(2015, Calendar.JUNE, 8, 20, 0);
+        filmshow.setDateTime(cal.getTime());
+        Film filmExpected = filmshow.getFilm();
+        Hall hallExpected = filmshow.getHall();
+        String dateTimeExpected = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshow.getDateTime());
+        filmshowDao.update(filmshow);
+        Filmshow filmshowTest = filmshowDao.getFilmshowById(filmshow.getFilmshowId());
+        Assert.assertNotNull(filmshowTest);
+        Film filmResult = filmshowTest.getFilm();
+        Hall hallResult = filmshowTest.getHall();
+        String dateTimeResult = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(filmshowTest.getDateTime());
+        Assert.assertEquals(filmExpected, filmResult);
+        Assert.assertEquals(hallExpected, hallResult);
+        Assert.assertEquals(dateTimeExpected, dateTimeResult);
+    }
+
+    @Test
+    public void testDelete() throws DaoException
+    {
+        Filmshow filmshow = new Filmshow();
+        filmshow.setFilmshowId(3);
+        filmshowDao.delete(filmshow);
+        Assert.assertNull(filmshowDao.getFilmshowById(filmshow.getFilmshowId()));
+    }
+
+    @Test
+    public void testGetFilmshowAll() throws DaoException
+    {
+        List<Filmshow> listTest = filmshowDao.getFilmshowAll();
+        Assert.assertNotNull(listTest);
+        Assert.assertTrue(listTest.size() > 0);
     }
 }
