801d9335aeb23e5aad9b131a4ef74e54a8b8bf7c
Oleg <89-oleg-2006@rambler.ru>
2015-09-08 20:45:51 +0300

    corrections after merge

--- sql/test_reservation.sql
+++
@@ -1,3 +0,0 @@
-Select login, price, time, film_name, hall_name, seat_number From Reservation R Inner Join Ticket T On R.ticket_id = T.ticket_id Inner Join User U On R.user_id = U.user_id
-Inner Join Filmshow F On T.filmshow_id = F.filmshow_id Inner Join Hall H On F.hall_id = H.hall_id
-Inner Join Seat S On T.seat_id = S.seat_id Inner Join Film Fi On F.film_id = Fi.film_id;
